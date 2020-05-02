3430b456cec95252bad3cce167f636d68a3ba52c
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-05-02 13:35:30 +0530

    Switched to public testnet

--- bluzelle/lib/Utils/BluzelleWrapper.dart
+++ bluzelle/lib/Utils/BluzelleWrapper.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class BluzelleWrapper{
-  static const String baseUrl = "http://testnet.private.bluzelle.com:1317";
+  static const String baseUrl = "http://testnet.public.bluzelle.com:1317";
   static Future<http.Response> getValidatorList() async {
     const url = baseUrl+"/staking/validators";
     var resp = await http.get(url);
